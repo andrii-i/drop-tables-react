3654449ed6fbbc1230355cea52f78e394a6142fc
Ishaan Karvir <ikarvir@calpoly.edu>
2022-06-07 16:45:02 -0700

    Fetch response options for both surveys

--- src/TakingURESurvey.js
+++ src/TakingURESurvey.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import NavBar from './NavBar';
-import {useParams} from "react-router-dom";
+import axios from 'axios';
+
 
 export default function TakingURESurvey() {
+  const [responseOptions, setOptions] = useState();
+  const surveyId = 1;
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:5000/get_response_options/${surveyId}`).then((data) => setOptions(data.data));
+  }, [])
   return (
     <Box className='container-center-horizontal' sx={{height: '100vh'}}>
       <NavBar />
       <div style={{color: "white"}}>
         This is take URE survey
+        {responseOptions && responseOptions.map((response) => <div style={{color:"white"}}>{response.Position}</div>)}
       </div>
     </Box>
   );
