cc1379b22c01e6de5619a272600afb24b5c9446f
Ishaan Karvir <ikarvir@calpoly.edu>
2022-06-07 14:55:16 -0700

    fetch and display both types of surveys

--- src/SurveysPage.js
+++ src/SurveysPage.js
@@ -1,14 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
 import NavBar from './NavBar';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
 
-export default function SurveysPage() {
-  
+function DisplaySurvey({survey}){
+  return (
+    <div style={{color:"white"}}>
+      {survey.Description}
+    </div>
+  )
+}
 
+export default function SurveysPage() {
+  const [surveys, setSurveys] = useState()
   useEffect(() => {
-    axios.get(`http://127.0.0.1:5000/get_surveys`).then((data) => console.log(data.data));
+    axios.get(`http://127.0.0.1:5000/get_surveys`).then((data) => setSurveys(data.data));
   }, []);
 
   return (
@@ -19,6 +26,8 @@ export default function SurveysPage() {
           Select a Survey to Take
         </Typography>
       </Box>
+      {surveys && surveys.map((survey) => <DisplaySurvey survey={survey}></DisplaySurvey>)}
+   
     </Box>
   )
 }
