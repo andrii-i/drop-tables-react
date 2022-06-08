e52ba503263c2429b239dd9cfcb53558fdcc7cc8
Ishaan Karvir <ikarvir@calpoly.edu>
2022-06-07 20:39:09 -0700

    Added an array containing all the answers

--- src/TakingURESurvey.js
+++ src/TakingURESurvey.js
@@ -9,11 +9,16 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Button from '@mui/material/Button';
 
-function DisplayQuestionOption({questionOpt}){
+function DisplayQuestionOption({questionOpt, setAnswers}){
   const [value, setValue] = useState();
-
   const handleChange = (event) => {
     setValue(event.target.value);
+    setAnswers(prev => prev.map(x => {
+      if(x.index == questionOpt.Position-1){
+        return {...x, answer: parseInt(event.target.value)}
+      }
+      return x;
+    }))
   };
 
   return (
@@ -23,12 +28,12 @@ function DisplayQuestionOption({questionOpt}){
         <RadioGroup
           aria-labelledby="radio-buttons-group"
           name="controlled-radio-buttons-group"
-          value={value}
+          value={value ? value : 0}
           onChange={handleChange}
           sx={{ mb: 5 }}
         >
           {questionOpt.responses.map(x => 
-            <FormControlLabel value={x.ResponsePrompt} control={<Radio />} label={x.ResponsePrompt} sx={{color: 'black', ml: 5}} />)}
+            <FormControlLabel value={x.ResponseValue} control={<Radio />} label={x.ResponsePrompt} sx={{color: 'black', ml: 5}} />)}
         </RadioGroup>
       </FormControl>
     </Box>
@@ -39,12 +44,13 @@ export default function TakingURESurvey() {
   const [responseOptions, setOptions] = useState();
   const [questions, setQuestions] = useState();
   const [questionOptions, setQOptions] = useState();
+  const [questionAnswers, setAnswers] = useState()
   const surveyId = 1;
+  console.log(questionAnswers);
   useEffect(() => {
     axios.get(`http://127.0.0.1:5000/get_response_options/${surveyId}`).then((data) => setOptions(data.data));
     axios.get(`http://127.0.0.1:5000/get_survey_questions/${surveyId}`).then((data) => setQuestions(data.data));
   }, [])
-  console.log(questionOptions);
 
   useEffect(() => {
     if(questions && responseOptions){
@@ -66,6 +72,8 @@ export default function TakingURESurvey() {
         
       }
       setQOptions(questionResponses);
+      let numQuestions = questions.length
+      setAnswers(Array(numQuestions).fill(0).map((val, index) => {return {index: index, answer: 0}}))
 
     }
   }, [questions])
@@ -76,7 +84,7 @@ export default function TakingURESurvey() {
         URE survey
         {/* {questions && questions.map((response) => <div style={{color:"white"}}>Question {response.Prompt}</div>)}
         {responseOptions && responseOptions.map((response) => <div style={{color:"white"}}>Response {response.ResponsePrompt}</div>)} */}
-        {questionOptions && questionOptions.map(x => <DisplayQuestionOption questionOpt={x}></DisplayQuestionOption>)}
+        {questionOptions && questionOptions.map(x => <DisplayQuestionOption setAnswers={setAnswers} questionOpt={x}></DisplayQuestionOption>)}
       </div>
       <Button
         variant='contained'
